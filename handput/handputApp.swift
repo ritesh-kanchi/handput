fd2fa3fe7a619e785e00c2daedc8956c7f544ad3
Ritesh Kanchi <ritesh.kanchi@hotmail.com>
2022-10-22 12:17:14 -0700

    Initial Commit

---
+++ handput/handputApp.swift
@@ -0,0 +1,17 @@
+//
+//  handputApp.swift
+//  handput
+//
+//  Created by Ritesh Kanchi on 10/22/22.
+//
+
+import SwiftUI
+
+@main
+struct handputApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
